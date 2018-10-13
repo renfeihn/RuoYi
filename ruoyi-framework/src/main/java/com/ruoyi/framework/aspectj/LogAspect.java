d83af40afd251b9fa927316dc53cbec49132a863
renfei <renfei_hn@163.com>
2018-10-14 03:11:05 +0800

    删除操作日志

--- ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/LogAspect.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/LogAspect.java
@@ -1,13 +1,6 @@
 package com.ruoyi.framework.aspectj;
 
 import com.ruoyi.common.annotation.Log;
-import com.ruoyi.common.enums.BusinessStatus;
-import com.ruoyi.common.json.JSON;
-import com.ruoyi.common.utils.StringUtils;
-import com.ruoyi.framework.util.ServletUtils;
-import com.ruoyi.framework.util.ShiroUtils;
-import com.ruoyi.system.domain.SysOperLog;
-import com.ruoyi.system.domain.SysUser;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -20,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
-import java.util.Map;
 
 /**
  * 操作日志记录处理
@@ -66,35 +58,6 @@ public class LogAspect {
                 return;
             }
 
-            // 获取当前的用户
-            SysUser currentUser = ShiroUtils.getUser();
-
-            // *========数据库日志=========*//
-            SysOperLog operLog = new SysOperLog();
-            operLog.setStatus(BusinessStatus.SUCCESS.ordinal());
-            // 请求的地址
-            String ip = ShiroUtils.getIp();
-            operLog.setOperIp(ip);
-
-            operLog.setOperUrl(ServletUtils.getRequest().getRequestURI());
-            if (currentUser != null) {
-                operLog.setOperName(currentUser.getLoginName());
-                if (StringUtils.isNotNull(currentUser.getDept())
-                        && StringUtils.isNotEmpty(currentUser.getDept().getDeptName())) {
-                    operLog.setDeptName(currentUser.getDept().getDeptName());
-                }
-            }
-
-            if (e != null) {
-                operLog.setStatus(BusinessStatus.FAIL.ordinal());
-                operLog.setErrorMsg(StringUtils.substring(e.getMessage(), 0, 2000));
-            }
-            // 设置方法名称
-            String className = joinPoint.getTarget().getClass().getName();
-            String methodName = joinPoint.getSignature().getName();
-            operLog.setMethod(className + "." + methodName + "()");
-            // 处理设置注解上的参数
-            getControllerMethodDescription(controllerLog, operLog);
             // 保存数据库
 //            AsyncManager.me().execute(AsyncFactory.recordOper(operLog));
         } catch (Exception exp) {
@@ -105,37 +68,6 @@ public class LogAspect {
         }
     }
 
-    /**
-     * 获取注解中对方法的描述信息 用于Controller层注解
-     *
-     * @return 方法描述
-     * @throws Exception
-     */
-    public void getControllerMethodDescription(Log log, SysOperLog operLog) throws Exception {
-        // 设置action动作
-        operLog.setBusinessType(log.businessType().ordinal());
-        // 设置标题
-        operLog.setTitle(log.title());
-        // 设置操作人类别
-        operLog.setOperatorType(log.operatorType().ordinal());
-        // 是否需要保存request，参数和值
-        if (log.isSaveRequestData()) {
-            // 获取参数的信息，传入到数据库中。
-            setRequestValue(operLog);
-        }
-    }
-
-    /**
-     * 获取请求的参数，放到log中
-     *
-     * @param operLog 操作日志
-     * @throws Exception 异常
-     */
-    private void setRequestValue(SysOperLog operLog) throws Exception {
-        Map<String, String[]> map = ServletUtils.getRequest().getParameterMap();
-        String params = JSON.marshal(map);
-        operLog.setOperParam(StringUtils.substring(params, 0, 255));
-    }
 
     /**
      * 是否存在注解，如果存在就获取
